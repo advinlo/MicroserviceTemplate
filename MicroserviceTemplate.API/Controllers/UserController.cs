9783331717c776eb041d4bbec5e96864a12b5e28
Louis A.D. <addo.lou@gmail.com>
2024-07-29 23:13:48 +0000

    Update classes to use MediatR

--- MicroserviceTemplate.API/Controllers/UserController.cs
+++ MicroserviceTemplate.API/Controllers/UserController.cs
@@ -1,31 +1,39 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using MicroserviceTemplate.Application.DTOs;
+using MicroserviceTemplate.Application.Exceptions;
+using MicroserviceTemplate.Application.Queries;
 
 namespace MicroserviceTemplate.API.Controllers
 {
 
     [ApiController]
-    [Route("/users/api/weatherController")]
+    [Route("/users/api/userController")]
     public class UserController : ControllerBase
     {
         private readonly ILogger<UserController> _logger;
-        //private readonly ISender _mediator;
+        private readonly ISender _mediator;
 
-        public UserController(ILogger<UserController> logger){
+        public UserController(ILogger<UserController> logger, ISender mediator){
             _logger = logger;
+            _mediator = mediator;
         }
+        
 
-
-
-        [HttpGet(Name = "GetUser")]
-        public UserDto Get()
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UserDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(Guid id)
         {
-            return new UserDto {
-                id = Guid.NewGuid(),
-                Username = "Advinlo",
-                EmailAddress = "advinlo@advinlo.com",
-                RegistrationDate = DateTime.Now            
-            };
+            try{
+                var client = await _mediator.Send(new GetUserQuery(id));
+                return Ok(client);  
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            
         }
     }
 }
--- MicroserviceTemplate.Application.UseCase/User/GetUserByIdUseCase.cs
+++ MicroserviceTemplate.Application.UseCase/User/GetUserByIdUseCase.cs
@@ -1,16 +1,42 @@
+using MediatR;
+using MicroserviceTemplate.Application.DTOs;
+using MicroserviceTemplate.Application.Queries;
+using MicroserviceTemplate.Application.Exceptions;
+
 using MicroserviceTemplate.Domain.Contract;
+using MicroserviceTemplate.Domain.Entities;
 
 
 namespace MicroserviceTemplate.Application.UseCase;
 
-public class GetUserByIdUseCase
+public class GetUserByIdUseCase : IRequestHandler<GetUserQuery, UserDto>
 {
 
-    private IUserService _userService;
+    //private IUserService _userService;
 
-    public GetUserByIdUseCase(IUserService userService) {
+    public GetUserByIdUseCase() {
 
-        _userService = userService;
+        //_userService = userService;
     }
+    
+    public async Task<UserDto> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            //var user = await _userService.Get(request.Id);
+            var userDto = new UserDto 
+            {
+                  Id = request.Id,
+                  Username = "Advinlo",
+                  EmailAddress = "advinlo@advinlo.com",
+                  RegistrationDate = DateTime.Now
+            };
 
+            // if (user == null)
+            //     throw new EntityNotFoundException("User", request.Id.ToString());
+
+            if (userDto == null)
+                throw new EntityNotFoundException("User", request.Id.ToString());
+            
+
+            return userDto;
+        }
 }
