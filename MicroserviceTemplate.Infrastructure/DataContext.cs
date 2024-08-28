e10d3cd1801530ed9f6cfd86d94b76f3a99329c4
Louis A.D. <addo.lou@gmail.com>
2024-08-28 09:16:18 +0000

    Update Infrastructure Layer

---
+++ MicroserviceTemplate.Infrastructure/DataContext.cs
@@ -0,0 +1,36 @@
+using MicroserviceTemplate.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace MicroserviceTemplate.Infrastructure;
+
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+
+    public void ApplyMigrations()
+    {
+        Database.Migrate();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+    }
+    public DbSet<User> Users { get; set; }    
+
+
+}
--- MicroserviceTemplate.Infrastructure/UserRepository.cs
+++ MicroserviceTemplate.Infrastructure/UserRepository.cs
@@ -1,31 +1,28 @@
 ﻿using MicroserviceTemplate.Domain.Entities;
 using MicroserviceTemplate.Application.Contract;
+using MicroserviceTemplate.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace MicroserviceTemplate.Infrastructure;
 
 public class UserRepository : IUserRepository
 {
-    public async Task<User> GetById(Guid id) 
-    {
-        User user = new User {
-            Id = id,
-            Username = "Advinlo",
-            EmailAddress = "advinlo@advinlo.com",
-            RegistrationDate = DateTime.Now
-        };
+    private readonly DataContext _dbContext;
 
-        return user; 
+    public UserRepository(DataContext dbContext){
+        _dbContext = dbContext;
     }
 
-    public async Task<User> GetByEmail(string emailAddress) 
+    public async Task<User?> GetById(Guid id) 
     {
-        User user = new User {
-            Id = Guid.NewGuid(),
-            Username = "Advinlo",
-            EmailAddress = emailAddress,
-            RegistrationDate = DateTime.Now
-        };
+        return await _dbContext.Set<User>()
+            .FirstOrDefaultAsync(u => u.Id == id);
+    }
 
-        return user; 
+    public async Task<User?> GetByEmail(string emailAddress) 
+    {
+        return await _dbContext.Set<User>()
+            .FirstOrDefaultAsync(u => u.EmailAddress == emailAddress); 
     }
 }
